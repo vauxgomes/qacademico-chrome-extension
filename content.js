9304374f3e0ee3d17cf91baea980bd38ed2488e5
Vaux Gomes <vauxsandino@gmail.com>
2025-07-03 22:06:20 -0300

    Filtering hidden inputs to fix reseting grades

--- content.js
+++ content.js
@@ -3,10 +3,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const notas = request.notas
     const inputs = document.querySelectorAll('input[name^="NOTA"]')
 
-    inputs.forEach((input, i) => {
-      if (i < notas.length) {
-        input.value = notas[i]
-      }
-    })
+    Array.from(inputs)
+      .filter((input) => input.type !== 'hidden')
+      .forEach((input, i) => {
+        if (i < notas.length) {
+          input.value = notas[i]
+        }
+      })
   }
 })
